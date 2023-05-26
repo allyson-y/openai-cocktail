4fc775a7724984ab3d54b9c126561e83a88e577d
allyson-y <ayao20@holynames-sea.org>
2023-05-26 13:24:58 -0700

    output is weirdly formatted

--- app.py
+++ app.py
@@ -10,26 +10,30 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        adj = request.form["adj"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
+            prompt=generate_prompt(adj),
             temperature=0.6,
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
+    print(result)
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+def generate_prompt(adj):
+    return f"""
+    Your task is to create an alcoholic coktail using the adjectives delimited by triple \
+    backticks.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+    Produce the following in a html format:
+    Name: <drink name that is creative and does not contain the original adjectives>
+
+    Ingredients: <drink ingredients (around 3-5)>
+
+    Directions: <step by step drink directions>
+
+    \"\"\"{adj}\"\"\"
+    """
